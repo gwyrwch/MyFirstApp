76675ef130d1f71318d8c763eb673a5b989fba63
gwyrwch <gwyrwch@gmail.com>
2019-10-02 12:56:05 +0300

    groovy script for get versionName added to project c:

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView versionName = findViewById(R.id.textName);
+        versionName.setText("version : " + BuildConfig.VERSION_NAME);
+
     }
+
+
 }
